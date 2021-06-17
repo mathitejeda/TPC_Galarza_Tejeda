cae0b6819d4bad3b9d7004c803055db5939a3da5
mathitejeda <mathi.tejeda@gmail.com>
2021-06-16 22:22:57 -0300

    Capa modelo Se crearon las clases base Se agregaron algunos atributos

---
+++ Modelo/Contacto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Contacto
+    {
+        string direccion { get; set; }
+        string telefono { get; set; }
+        string celular { get; set; }
+        string codigoPostal { get; set; }
+    }
+}
---
+++ Modelo/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Persona
+    {
+        int Id { get; set; }
+        string Nombre { get; set; }
+        string Apellido { get; set; }
+        string DNI { get; set; }
+        Contacto Contacto { get; set; }
+    }
+}
