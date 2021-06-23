1e898d46bab0058727646749b86bf060fcf081aa
Cecilia Galarza <81053524+cecilia-galarza@users.noreply.github.com>
2021-06-22 22:13:30 -0300

    Creacion Clase ListaTickets

---
+++ Modelo/ListaTicket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ListaTicket
+    {
+        public int IdTicket { get; set; }
+        public int IDEstado { get; set; }
+        public int IdCliente { get; set; }
+        public int IdTecnico { get; set; }
+        public int IdProducto { get; set; }
+        public String Estado { get; set; }
+        public String Cliente { get; set; }
+        public String Tecnico { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public String Producto { get; set; }
+        
+    }
+}
