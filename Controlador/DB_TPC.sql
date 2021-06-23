3766e48fd86bd58dea2664554cca1071d1bee058
Mathias Tejeda <mathi.tejeda@gmail.com>
2021-06-22 22:01:22 -0300

    Base de datos final

--- Controlador/DB_TPC.sql
+++ Controlador/DB_TPC.sql
@@ -4,55 +4,16 @@ GO
 
 create table areas
 (
-    IDArea int PRIMARY KEY,
+    IDArea int PRIMARY KEY identity(1,1),
     nombre varchar(10) not null,
     descripcion VARCHAR(100) not null
 )
 
 GO
 
-create table tecnicos
-(
-    IDTecnico bigint PRIMARY KEY,
-    IDArea int FOREIGN key REFERENCES areas(IDArea) not null,
-    nombre varchar(30) not null,
-    apellido varchar(30) not null,
-    DNI varchar (10) null unique,
-    telefono varchar(15) null,
-    celular varchar(15) null,
-    mail varchar(50) null
-)
-
-GO
-
-create table supervisores
-(
-    IDSupervisor bigint PRIMARY KEY,
-    IDArea int FOREIGN KEY REFERENCES areas(IDArea) not null,
-    nombre varchar(30) not null,
-    apellido VARCHAR(30) not null,
-    dni varchar(10) not null,
-    telefono varchar(15) null,
-    celular varchar(15) null,
-    mail varchar(50) null
-)
-
-GO
-
-create table clientes
-(
-    IDCliente bigint PRIMARY KEY,
-    razonSocial varchar(10) not null,
-    telefono varchar(15) null,
-    celular varchar(15) null,
-    mail varchar(50) null
-)
-
-GO
-
 create table productos
 (
-    IDProducto int PRIMARY KEY,
+    IDProducto bigint PRIMARY KEY,
     nombre varchar(15) not null,
     numeroSerie varchar(15) not null,
     descripcion varchar(100) not null
@@ -60,14 +21,84 @@ create table productos
 
 GO
 
-create table incidencias
+create table tipoUsuario
 (
-    IDIncidencia int PRIMARY KEY,
-    IDProducto int FOREIGN KEY references productos(IDProducto),
-    IDCliente bigint FOREIGN KEY references clientes(IDCliente),
-    IDTecnico bigint FOREIGN KEY REFERENCES tecnicos(IDtecnico),
-    detalle varchar(100),
-    estado char not null,
-    fechaIngreso date not null,
-    fechaEgreso date null
+    IDTipo int PRIMARY KEY,
+    Descripcion varchar(30) not null
 )
+
+GO
+
+create table usuarios
+(
+    IDUsuario bigint PRIMARY KEY identity(1,1),
+    IDTipo int FOREIGN key REFERENCES tipoUsuario(IDTipo) not null,
+    nombre varchar(30) not null,
+    apellido varchar(30) not null,
+    DNI varchar (10) not null unique
+)
+
+GO
+
+create table contacto
+(
+	IDContacto bigint primary key identity(1,1),
+	IDUsuario bigint foreign key references usuarios(IDUsuario),
+	domicilio varchar(50) not null,
+	telefono varchar(50) not null,
+	celular varchar(50) not null,
+	mail varchar(50) not null
+)
+
+go
+
+CREATE TABLE Autenticaciones
+(
+    IDUsuario bigint FOREIGN KEY references Usuarios(IDUsuario)not null,
+    Contrasenia varchar(100) not null,
+	Primary key(IDUsuario)
+)
+
+go
+
+create table ticket(
+   IDTicket int PRIMARY KEY identity(1,1),
+    IDTecnico bigint FOREIGN KEY REFERENCES Usuarios(IDUsuario)not null,
+	IDProducto bigint FOREIGN KEY REFERENCES Productos(IDProducto) not null,
+	IDEstado int not null,
+    Problema varchar(300) null,
+	Diagnostico varchar(300) null,
+	Solucion varchar(300) null,
+    FechaIngreso date not null,
+    FechaEgreso date null
+)
+
+go
+
+create table estadoTicket(
+	IDEstado int primary key references ticket(IDTicket),
+	nombre varchar(50) not null
+)
+
+GO
+
+create table productosPorCliente(
+	IDProducto bigint not null,
+	IDCliente bigint not null,
+	primary key(IDProducto, IDCliente),
+	foreign key (IDProducto) references productos(IDProducto),
+	foreign key (IDCliente) references usuarios(IDUsuario)
+)
+
+go
+
+create table usuariosPorArea
+(
+	IDUsuario bigint,
+	IDArea int,
+	primary key(IDUsuario, IDarea),
+	foreign key(IDUsuario) references usuarios(IDUsuario),
+	foreign key (IDArea) references areas(IDArea)
+)
+
+GO
